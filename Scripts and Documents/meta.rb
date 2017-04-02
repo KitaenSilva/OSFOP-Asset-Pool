d4b62e7a4c2380e8d54ccc016966f5a4c913f22e
jonathan de jong <jonathan de jong>
2017-04-03 00:20:25 +0200

    more stuff

--- Scripts and Documents/meta.rb
+++ Scripts and Documents/meta.rb
@@ -38,6 +38,11 @@ class Meta
         y = 0 if y < 0;x = 0 if x < 0
         MOVW.call(SELF_WINDOW,x+dx,y+dy,width,height,0)
     end
+    def self.movetocoords(x,y)
+        width = 544 + ((SMET.call(5) + SMET.call(45)) * 2)
+        height = (SMET.call(6) + SMET.call(45)) * 2 + SMET.call(4) + 416
+        MOVW.call(SELF_WINDOW,x-8,y-8,width,height,0)
+    end
     def self.getwpos
         r = RECT.pack("llll")
         GWRF.call(SELF_WINDOW,r)
@@ -48,36 +53,63 @@ class Meta
     end
     def self.ree(amount)
         amount.times do |i|
-            Schedule_buffer[:shift] << Schedule_function.new("winmov",[5,5])
-            Schedule_buffer[:shift] << Schedule_function.new("winmov",[-10,-10])
-            Schedule_buffer[:shift] << Schedule_function.new("winmov",[0,10])
-            Schedule_buffer[:shift] << Schedule_function.new("winmov",[10,-10])
-            Schedule_buffer[:shift] << Schedule_function.new("winmov",[-5,5])
+            $Schedule_buffer[:shift] << Schedule_function.new("winmov",[5,5])
+            $Schedule_buffer[:shift] << Schedule_function.new("winmov",[-10,-10])
+            $Schedule_buffer[:shift] << Schedule_function.new("winmov",[0,10])
+            $Schedule_buffer[:shift] << Schedule_function.new("winmov",[10,-10])
+            $Schedule_buffer[:shift] << Schedule_function.new("winmov",[-5,5])
         end
+    end
+    def self.shake(shakestuffz)
+        if (shakestuffz[2] >= shakestuffz[1]) && shakestuffz[1] != -1
+            $AllDoTheHarlemShake = false
+            self.movetocoords(shakestuffz[3][0], shakestuffz[3][1])
+            return
+        end
+        r = Random.new
+        xtens = r.rand(shakestuffz[0])
+        ytens = r.rand(shakestuffz[0])
+        xtens = xtens * -1 if r.rand(2) == 1; ytens = ytens * -1 if r.rand(2) == 1
+        self.movetocoords(xtens + shakestuffz[3][0],ytens + shakestuffz[3][1])
+        $ShakeMusic[2] += 1
+    end
+    def self.startshake(intensity, amount)
+        $ShakeMusic = [intensity,amount,0,self.getwpos]
+        $AllDoTheHarlemShake = true
+    end
+    def self.stopshake
+        $AllDoTheHarlemShake = false
+        self.movetocoords($ShakeMusic[3][0], $ShakeMusic[3][1])
     end
 end
 
 Schedule_function = Struct.new(:type, :vars)
-Schedule_buffer = { :shift => [], :full => [] }
+$Schedule_buffer = { :shift => [], :full => [] }
+$AllDoTheHarlemShake = false
+$ShakeMusic = [0,0,0,[0,0]] #intensity, shaking amount, shakes done, original coords, opt(shaketype, extrainfo)
 
 class Schedule
     def self.run
-        if !Schedule_buffer[:shift].empty?
-            task = Schedule_buffer[:shift].shift
+        if !$Schedule_buffer[:shift].empty?
+            task = $Schedule_buffer[:shift].shift
             case task[:type]
         when "winmov"
             Meta.move(task[:vars][0],task[:vars][1])
+            end
         end
-        if !Schedule_buffer[:full].empty?
+        if !$Schedule_buffer[:full].empty?
             i = 0
-            fi = Schedule_buffer[:full].length
+            fi = $Schedule_buffer[:full].length
             while i < fi 
-                task = Schedule_buffer[:full].shift
-                case task[:type]
+                task = $Schedule_buffer[:full].shift
+                #case task[:type]
                     # nothing yet
-                end
+                #end
                 i += 1
             end
+        end
+        if $AllDoTheHarlemShake
+            Meta.shake($ShakeMusic)
         end
     end
 end
